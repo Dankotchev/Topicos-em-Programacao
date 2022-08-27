afc2113c9a9bce94d5da47b77f97dda7e7450f49
Dankotchev <dankotchev.tech@outlook.com>
2022-08-27 13:34:31 -0300

    Continuando o problema Varetas

--- TPRC0 02 - Lista de Exercícios: Aquecimento V INTERIF/varetas.py
+++ TPRC0 02 - Lista de Exercícios: Aquecimento V INTERIF/varetas.py
@@ -2,26 +2,29 @@ jogadores = ['Jogador 1', 'Jogador 2', 'Jogador 3']
 pontuacao = [0, 0, 0]
 varetas = [5, 10, 15, 20, 50]
 rodada = 1
-print(varetas)
+
+# def ganhador ():
+
 
 while True:
     jogadorInicial = int(input("Jogador Inicial da Rodada: "))
-    if  jogadorInicial > -1:
-        jogo = [int(valores) for valores in input("Jogo: ").split(" ")]
+    if jogadorInicial > -1:
+        partida = [int(valores) for valores in input("Jogo: ").split(" ")]
         print("RODADA {}".format(rodada))
-        print(jogo)
-    for indice in jogo:
-        if indice != 0:
-            pontuacao[jogadorInicial -1 ] = pontuacao[jogadorInicial -1] + varetas[indice - 1]
-            #print(varetas[i-1])
-            #print(i)
-        if indice == 0:
-            jogadorInicial += 1
-        if jogadorInicial == 4:
-            jogadorInicial = 1
-        if indice == -1:
-            break
-    for indice, valor in jogadores:
-        print("{} fez {} pontos".format(valor, pontuacao[indice]))
+        for i in partida:
+            if i != 0:
+                pontuacao[jogadorInicial - 1] += varetas[i - 1]
+            if i == 0:
+                jogadorInicial += 1
+                if jogadorInicial == 4:
+                    jogadorInicial = 1
+            if i == -1:
+                break
+        vencedor = pontuacao.index(max(pontuacao))  # Verifica a posição do vencedor
+        # Não verifica se ocorre empates
+        print("Ganhador com {} pontos".format(max(pontuacao)))
+        print("{}".format(jogadores[vencedor]))
+    pontuacao = [0, 0, 0]  # Resetando pontuações após a finalização de uma partida
+    rodada += 1
     if jogadorInicial == -1:
         break
