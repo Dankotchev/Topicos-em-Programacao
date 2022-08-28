fdc37b10f3a1ab0f6e5805e92cffd29d1165871c
Dankotchev <dankotchev.tech@outlook.com>
2022-08-28 15:21:01 -0300

    Problema trade lê um arquivo de mensagens cifradas e grava o resultado em um novo arquivo

--- TPRC0 02 - Lista de Exercícios: Aquecimento V INTERIF/trade.py
+++ TPRC0 02 - Lista de Exercícios: Aquecimento V INTERIF/trade.py
@@ -3,45 +3,9 @@ from builtins import len
 def binaryToDecimal(binario):
     return int(binario, 2) if binario != "" else 0
 
-def decimalToBinary(decimal):
-    binario = ''
-    while decimal > 0:
-        binario += str(decimal % 2)
-        decimal //= 2
-    while len(binario) < 8:
-        binario += '0'
-    return binario[::-1]
-
 def traduza(entrada):
   volta = "".join(["0" if x.islower() else "1" for x in entrada])
   return volta
-
-def criptografar (textoTransporte):
-    segredo = input("Segredo: ")
-    fraseFinal = []
-    binaryText = ''
-
-    # Transformando a frase segredo em uma string de 0 e 1
-    for letra in segredo:
-        binary = decimalToBinary(ord(letra))  # Uma letra é transformada em um string de 0/1
-        binaryText += binary
-    posicao = 0
-    # Cifrando a mensagem no texto de Transporte
-    for l in textoTransporte:
-        if l != ' ':
-            if posicao > len(binaryText) - 1:
-                fraseFinal.append(
-                    l.lower())  # Caso o texto de transporte seja maior que o segredo, todas as letras ficam minusculas
-            else:
-                if binaryText[posicao] == "0":
-                    fraseFinal.append(l.lower())
-                else:
-                    fraseFinal.append(l.upper())
-            posicao += 1
-        else:
-            fraseFinal.append(l)
-    print("".join(fraseFinal))
-
 
 def descriptografar(textoTransporte):
     fraseFinal = []
@@ -50,15 +14,18 @@ def descriptografar(textoTransporte):
         caracter = binaryToDecimal(traduza(textoTransporte[x - 8:x]))
         if caracter != 0:
             fraseFinal.append(chr(caracter))
-    print("".join(fraseFinal))
+    return "".join(fraseFinal)
 
 
-print("Escolha uma opção...\n1- Criptografar uma mensagem\n2- descriptografar uma mensagem")
-escolha = int(input("ESCOLHA :: "))
-textoTransporte = input("Texto de Transporte: ")
+arquivoEntrada = "EntradasCodificadas.txt"
+with open(arquivoEntrada) as arq:
+    linhas = arq.read().split('\n')
 
-if escolha == 1:
-    criptografar(textoTransporte)
-else escolha == 2:
-    descriptografar(textoTransporte)
+with open ("SaidasDecodificadas.txt", "w") as saida:
+    for l in linhas:
+        descifrado = descriptografar(l)
+        print(descifrado)
+        saida.write(descifrado)
+saida.close()
+arq.close()
 
