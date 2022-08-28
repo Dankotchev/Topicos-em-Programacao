458835673327c3c94de0c95c0958cee305dd67d8
Dankotchev <dankotchev.tech@outlook.com>
2022-08-28 17:15:59 -0300

    Função que define o ganhador ou empates e gravação dos resultados em arquivo

--- TPRC0 02 - Lista de Exercícios: Aquecimento V INTERIF/varetas.py
+++ TPRC0 02 - Lista de Exercícios: Aquecimento V INTERIF/varetas.py
@@ -1,10 +1,24 @@
+def ganhador (pontuacoes, jogadores):
+    if pontuacoes.count(max(pontuacoes)) == 1:  # Apenas uma ocorrência do MAIOR valor, logo um ganhador
+        resultado = "Ganhador com {} pontos".format(max(pontuacoes)) + "\n"
+        resultado += "{}".format(jogadores[pontuacoes.index(max(pontuacoes))])
+    else:
+        resultado = "Empate com {} pontos".format(max(pontuacao)) + "\n"
+        for i in range(len(pontuacoes)):
+            if pontuacoes[i] == max(pontuacoes):
+                resultado += "{}, ".format(jogadores[i])
+    return resultado + "\n"
+
+
 jogadores = ['Jogador 1', 'Jogador 2', 'Jogador 3']
-pontuacao = [0, 0, 0]
 varetas = [5, 10, 15, 20, 50]
 rodada = 1
+jogadorInicial = 0
+strIntermediaria = ''
 
 with open ("SaidasResultados.txt", "w") as saida:
-    while True:
+    while jogadorInicial != -1:
+        pontuacao = [0, 0, 0]  # Resetando pontuações após a finalização de uma partida
         jogadorInicial = int(input("Jogador Inicial da Rodada: "))
         if jogadorInicial > -1:
             partida = [int(valores) for valores in input("Jogo: ").split(" ")]
@@ -17,19 +31,8 @@ with open ("SaidasResultados.txt", "w") as saida:
                     jogadorInicial += 1
                     if jogadorInicial == 4:
                         jogadorInicial = 1
-            vencedor = pontuacao.index(max(pontuacao))  # Verifica a posição do vencedor
-            # Não verifica se ocorre empates
-            strFinal = "RODADA {}".format(rodada) + "\n"
-            #print(strRodada)
-            strFinal += "Ganhador com {} pontos".format(max(pontuacao)) + "\n"
-            #print(strGanhador)
-            strFinal += "{}".format(jogadores[vencedor]) + "\n\n"
-            print(strFinal)
-            # print("Ganhador com {} pontos".format(max(pontuacao)))
-            # print("{}".format(jogadores[vencedor]))
-        pontuacao = [0, 0, 0]  # Resetando pontuações após a finalização de uma partida
+            strIntermediaria += "RODADA {}".format(rodada) + "\n" + ganhador(pontuacao, jogadores) + "\n"
         rodada += 1
-        saida.write(strFinal)
-        if jogadorInicial == -1:
-            break
+    print(strIntermediaria)
+    saida.write(strIntermediaria)
 saida.close()
